d06a34981c309401e991eb0c23843db7d823d7fb
deluxc <136242540+StabeW@users.noreply.github.com>
2024-03-22 16:45:40 +0200

    feature/add-entity-configuration
    add configuration

---
+++ StellarGoodsWebApi/StellarGoodsData/EntityConfiguration/OrderConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StellarGoodsData.Models;
+
+namespace StellarGoodsData.EntityConfiguration
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasKey(o => o.Id);
+
+            builder
+                .HasOne(u => u.User)
+                .WithMany(o => o.Orders)
+                .HasForeignKey(u => u.UserId);
+        }
+    }
+}
---
+++ StellarGoodsWebApi/StellarGoodsData/EntityConfiguration/OrderItemConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StellarGoodsData.Models;
+
+namespace StellarGoodsData.EntityConfiguration
+{
+    public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+    {
+        void IEntityTypeConfiguration<OrderItem>.Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.HasKey(o => new { o.OrderId, o.ProductId });
+
+            builder
+                .HasOne(o => o.Order)
+                .WithMany(i => i.OrderItems)
+                .HasForeignKey(o => o.OrderId);
+
+            builder
+                .HasOne(p => p.Product)
+                .WithMany(i => i.OrderItems)
+                .HasForeignKey(p => p.ProductId);
+        }
+    }
+}
